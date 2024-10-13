cb52a5933a76d55df765c9e70f6912f1bbff0667
tinalin728 <yuting.lin728@gmail.com>
2024-10-13 13:35:17 -0700

    created productlisting page

--- src/js/productListing.js
+++ src/js/productListing.js
@@ -1,5 +1,25 @@
-import { setupHamburgerMenu } from "./modules/menu";
+import { setupHamburgerMenu } from "./modules/menu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     setupHamburgerMenu();
+
+
+    const toggleOptions = document.querySelector('.js-options-toggle');
+    const optionsMenu = document.querySelector('.js-options-menu');
+    const optionsArrow = document.querySelector('.js-option-icon');
+    const selectedOption = document.querySelectorAll('.js-option-selected');
+
+    toggleOptions.addEventListener('click', () => {
+        optionsMenu.classList.toggle('hidden');
+        optionsArrow.classList.toggle('fa-angle-up');
+    })
+
+    selectedOption.forEach((option) => {
+        option.addEventListener('click', () => {
+            toggleOptions.querySelector('span').textContent = option.textContent;
+            optionsMenu.classList.add('hidden');
+            optionsArrow.classList.remove('fa-angle-up');
+
+        })
+    })
 })
